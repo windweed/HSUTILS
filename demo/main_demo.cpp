f753adfe4b47ab52bb52af1aacb85199c9572227
czy <ziijchen@163.com>
2021-01-18 17:23:08 +0800

    improve the CMakeLists.txt's ARCH

--- demo/main_demo.cpp
+++ demo/main_demo.cpp
@@ -9,8 +9,9 @@
 using namespace std;
 
 static void demo_cfg_file();
-
 static void demo_serialized_db();
+// helper
+static void fill_demo_hdr(struct ZiEncryptHdr*);
 
 int main(int argc, char* argv[])
 {
@@ -31,9 +32,9 @@ static void demo_cfg_file()
     // serialize
     // with header
     struct ZiEncryptHdr dbinfo;
-    FillHeader(&dbinfo);
+    fill_demo_hdr(&dbinfo);
     bool serialize_res = ZiSaveDatabase(worker.getDB(), &dbinfo,
-                                      (db_out_dir + "out.db").c_str());
+                                      (db_out_dir + "demo.db").c_str());
     printf("[ Info ] Serialize %s\n", serialize_res ? "Successfully" : "Failed");
     // @TEST without header
     // bool pure_seria_res = ZiSaveDatabase(worker.getDB(), nullptr,
@@ -47,9 +48,9 @@ static void demo_serialized_db()
     Worker worker;
     // with header info
     struct ZiEncryptHdr dbinfo;
-    worker.initBySerializedDB((db_dir + "out.db").c_str(), true, &dbinfo);
+    worker.initBySerializedDB((db_dir + "demo_in.db").c_str(), true, &dbinfo);
 
-    printf("[ Info ] Programm Version: %d.%d.%d\n",
+    printf("[ Info ] Demo Programm Version: %d.%d.%d\n",
         dbinfo.ver_major, dbinfo.ver_minor, dbinfo.ver_patch);
 
     time_t time = (time_t) dbinfo.btime;
@@ -63,4 +64,12 @@ static void demo_serialized_db()
     struct TestFlow flow { "hell0", 5 };
     uint id = worker.queryDB(&flow);
     id != 0U ? printf("Query OK, id is '%u'\n", id) : printf("Query Failed.\n");
+}
+
+static void fill_demo_hdr(struct ZiEncryptHdr* hdr)
+{
+    hdr->btime = DEMO_BUILD_TIME;
+    hdr->ver_major = DEMO_VER_MAJOR;
+    hdr->ver_minor = DEMO_VER_MINOR;
+    hdr->ver_patch = DEMO_VER_PATCH;
 }
