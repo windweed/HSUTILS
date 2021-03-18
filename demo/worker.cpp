0c842558720756d0c560482372351f208aecce04
czy <ziijchen@163.com>
2021-03-18 10:21:39 +0800

    .clang-format

--- demo/worker.cpp
+++ demo/worker.cpp
@@ -1,33 +1,31 @@
 #include "worker.h"
-#include "serializer.h"
 #include "deserializer.h"
 #include "encrypt_hdr.h"
+#include "serializer.h"
 // ISO
-#include <fstream>
-#include <vector>
-#include <string>
 #include <cassert>
+#include <fstream>
+#include <string>
+#include <vector>
 // POSIX
-#include <sys/mman.h>
 #include <sys/fcntl.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h> // close
 
 using namespace std;
 
-Worker::Worker()
-    : db_(nullptr)
-    , dbsize_(0)
+Worker::Worker() : db_(nullptr), dbsize_(0)
 {
-    for (int i = 0; i < TEST_MAX_SC; i++)
-        { sc_[i] = nullptr; }
+    for (int i = 0; i < TEST_MAX_SC; i++) {
+        sc_[i] = nullptr;
+    }
     printf("Worker Constructed\n");
 }
 
 Worker::~Worker()
 {
-    for (int i = 0; i < TEST_MAX_SC; i++)
-    {
+    for (int i = 0; i < TEST_MAX_SC; i++) {
         hs_free_scratch(sc_[i]);
     }
     hs_free_database(db_);
@@ -43,8 +41,7 @@ void Worker::initByCfgFile(const char* filename)
     ZiAllocScratchs(db_, sc_, TEST_MAX_SC, "txtcfg");
 }
 
-void Worker::initBySerializedDB(const char* filename, bool has_header,
-    struct ZiEncryptHdr* header)
+void Worker::initBySerializedDB(const char* filename, bool has_header, struct ZiEncryptHdr* header)
 {
     db_ = ZiLoadDatabase(filename, has_header, header);
     ZiAllocScratchs(db_, sc_, TEST_MAX_SC, "SerializedDB");
@@ -55,25 +52,24 @@ unsigned int Worker::queryDB(const struct TestFlow* flow, int thread_idx)
     return ZiScanDB(flow->data, db_, sc_[thread_idx]);
 }
 
-
 void Worker::parseCfgFile(const char* file, struct ZiHSCollData& data)
 {
     printf("Reading file '%s'...\n", file);
-    ifstream in_file { file };
+    ifstream in_file{file};
     if (!in_file.good()) {
         fprintf(stderr, "Read file '%s' Failed.\n", file);
         return;
     }
 
     for (int i = 0; !in_file.eof(); i++) {
-        string line {};
+        string line{};
         getline(in_file, line);
 
         if (line.empty() || line[0] == '#') {
             continue;
         }
 
-        string pcre {};
+        string pcre{};
         int id;
         extractPcreAndId(line, pcre, id);
 
@@ -85,12 +81,12 @@ void Worker::parseCfgFile(const char* file, struct ZiHSCollData& data)
     dbsize_ = data.patterns.size();
 }
 
-
 /**
  * @example 1,hello,\w+
-*/
-void Worker::extractPcreAndId(string line, string& pcre, int& id) {
-    vector<string> sp {};
+ */
+void Worker::extractPcreAndId(string line, string& pcre, int& id)
+{
+    vector<string> sp{};
     SplitString(line, ',', sp);
     id = stoi(sp[0]);
     if (!sp[2].empty()) {
@@ -109,23 +105,20 @@ hs_database_t* Worker::loadDBByMmap(const char* filename)
 {
     char* bytes = nullptr;
     int fd = open(filename, O_RDONLY);
-    if (fd < 0)
-    {
+    if (fd < 0) {
         return nullptr;
     }
 
     struct stat st;
-    if (fstat(fd, &st) < 0)
-    {
+    if (fstat(fd, &st) < 0) {
         close(fd);
         return nullptr;
     }
 
     size_t len = st.st_size;
 
-    bytes = (char*) mmap(nullptr, len, PROT_READ, MAP_SHARED, fd, 0);
-    if (bytes == MAP_FAILED)
-    {
+    bytes = (char*)mmap(nullptr, len, PROT_READ, MAP_SHARED, fd, 0);
+    if (bytes == MAP_FAILED) {
         fprintf(stderr, "mmap failed.\n");
         close(fd);
         return nullptr;
